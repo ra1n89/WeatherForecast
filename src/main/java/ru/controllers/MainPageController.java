c88c35addbac87637c192c7db897913bb2c0e68d
ra1n89nout <prorain88@gmail.com>
2025-01-03 21:42:14 +0500

    feat: add main page and MainController

--- src/main/java/ru/controllers/MainPageController.java
+++ src/main/java/ru/controllers/MainPageController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class FirstController {
+public class MainPageController {
 
-    @GetMapping("hello")
-    public String FirstMethod(){
-        return "first/hello";
+    @GetMapping("/")
+    String mainPage(){
+        return "index";
     }
 }
