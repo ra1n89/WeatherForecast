d031c444725a9106efa9d53bbb121c47c3574b7b
ra1n89nout <prorain88@gmail.com>
2025-01-03 18:12:09 +0500

    init: first spring web configuration

---
+++ src/main/java/ru/Main.java
@@ -0,0 +1,12 @@
+package ru;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
