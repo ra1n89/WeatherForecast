dc7722210e81f8387bca55cf11235d895e4a071b
ra1n89nout <prorain88@gmail.com>
2025-01-04 21:58:55 +0500

    feat: add Flyway config and sql script

---
+++ src/main/java/ru/config/FlyConfig.java
@@ -0,0 +1,22 @@
+package ru.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class FlyConfig {
+
+
+    @Bean
+    public Flyway flyway(DataSource dataSource){
+        Flyway flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:db/migration")
+                .load();
+        return flyway;
+    }
+}
--- src/main/java/ru/controllers/MainPageController.java
+++ src/main/java/ru/controllers/MainPageController.java
@@ -1,13 +1,19 @@
 package ru.controllers;
 
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainPageController {
 
+    @Autowired
+    Flyway flyway;
+
     @GetMapping("/")
     String mainPage(){
+        flyway.migrate();
         return "index";
     }
 }
