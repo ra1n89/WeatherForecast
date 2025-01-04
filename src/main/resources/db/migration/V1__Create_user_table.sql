09690327a14124f5156aa7bb19af48f857f1e6d6
ra1n89nout <prorain88@gmail.com>
2025-01-04 23:27:09 +0500

    refactor: add unique to some fields in DDL sql scripts

--- src/main/resources/db/migration/V1__Create_user_table.sql
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users
 (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(50) NOT NULL
 );
--- src/main/resources/db/migration/V3__Create_sessions_table.sql
+++ src/main/resources/db/migration/V3__Create_sessions_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE sessions(
-    id varchar(36) PRIMARY KEY,
+    id varchar(36) PRIMARY KEY UNIQUE,
     user_id INT NOT NULL ,
     expires_at DATETIME NOT NULL ,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
