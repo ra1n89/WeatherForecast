44155ee3e0b706e689ed6a983795de31a3db504b
ra1n89nout <prorain88@gmail.com>
2025-01-05 11:37:43 +0500

    feat: implement methods

--- src/main/java/ru/service/UserRepositoryService.java
+++ src/main/java/ru/service/UserRepositoryService.java
@@ -1,4 +1,33 @@
 package ru.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.repository.UserRepository;
+import ru.repository.entity.User;
+
+import java.util.List;
+
+@Service
 public class UserRepositoryService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User save(User user) {
+
+        return userRepository.save(user);
+    }
+
+    public User update(User user) {
+        return null;
+    }
+
+    public List<User> getAll() {
+        return null;
+    }
+
+    public User getOne(User user) {
+
+        return userRepository.getOne(user);
+    }
 }
