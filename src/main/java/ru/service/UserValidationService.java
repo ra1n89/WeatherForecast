1004966f78b5c7102dd75534b21afc128fd91aa2
ra1n89nout <prorain88@gmail.com>
2025-01-06 00:05:31 +0500

    feat: add validation

--- src/main/java/ru/service/UserValidationService.java
+++ src/main/java/ru/service/UserValidationService.java
@@ -1,13 +1,29 @@
 package ru.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import ru.repository.entity.User;
 
 @Service
 public class UserValidationService {
 
-    public boolean isUserValid(String name, String password){
-        if(name.equals("admin") && password.equals("123")) {
+    @Autowired
+    UserRepositoryService userRepositoryService;
+
+    public boolean isUserValid(String username, String password){
+        User user = userRepositoryService.getOne(new User(username, password));
+        if (user == null){
+            return false;
+        }
+        if(user.getUsername().equals(username) && user.getPassword().equals(password)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isPasswordEqual(String password, String repeatPassword){
+        if(password.equals(repeatPassword)) {
             return true;
         }
         return false;
