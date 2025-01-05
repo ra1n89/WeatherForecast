d3ffea7b9ae61ad08f86a63b5400245c2e7c564f
ra1n89nout <prorain88@gmail.com>
2025-01-05 11:38:48 +0500

    fix: add nessesary annotation and constructor without id

--- src/main/java/ru/repository/entity/User.java
+++ src/main/java/ru/repository/entity/User.java
@@ -2,12 +2,16 @@ package ru.repository.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -17,4 +21,8 @@ public class User {
 
     String password;
 
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
