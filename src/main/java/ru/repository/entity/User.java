76190fd9bab5d454b92c9594df410756c9b0fdfb
ra1n89nout <prorain88@gmail.com>
2025-01-05 12:27:16 +0500

    refactor: add Table annotation with reference to table

--- src/main/java/ru/repository/entity/User.java
+++ src/main/java/ru/repository/entity/User.java
@@ -2,6 +2,7 @@ package ru.repository.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "users")
 public class User {
 
     @Id
