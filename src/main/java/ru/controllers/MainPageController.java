d838a9cca6c13d60a9366cbf1746f55cbb920f25
ra1n89nout <prorain88@gmail.com>
2025-01-06 16:01:41 +0500

    feat: add http request to weather supplier API

--- src/main/java/ru/controllers/MainPageController.java
+++ src/main/java/ru/controllers/MainPageController.java
@@ -3,7 +3,17 @@ package ru.controllers;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.model.WeatherFoundByCity;
+import ru.util.JsonUtil;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @Controller
 public class MainPageController {
@@ -11,10 +21,31 @@ public class MainPageController {
     @Autowired
     Flyway flyway;
 
+
+
     @GetMapping("/")
-    String mainPage(){
+    String mainPage() {
         //TODO: перенести в сервлетЛистенер
         flyway.migrate();
         return "index";
+    }
+
+    @GetMapping("search-results")
+    String searchResult(@RequestParam("search_filter") String searchLocationFilter,
+                        Model model)
+            throws IOException, InterruptedException {
+
+        HttpClient httpClient = HttpClient.newBuilder().build();
+        HttpRequest request = HttpRequest.newBuilder(
+                URI.create("https://api.openweathermap.org/data/2.5/weather?q=" +
+                searchLocationFilter + "&appid=9aa0aba07c5c897aaf5cf0454766edb7")).GET().build();
+
+        HttpResponse<String> stringHttpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        WeatherFoundByCity weatherFoundByCity = JsonUtil.stringToJson(stringHttpResponse.body());
+        System.out.println(weatherFoundByCity);
+
+        model.addAttribute("searchResults", stringHttpResponse.body());
+
+        return "search-results";
     }
 }
