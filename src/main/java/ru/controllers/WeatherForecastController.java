615c1b703b49664a4a057814d55565c03cd4ba52
ra1n89nout <prorain88@gmail.com>
2025-01-06 16:03:58 +0500

    feat: some changes

--- src/main/java/ru/controllers/WeatherForecastController.java
+++ src/main/java/ru/controllers/WeatherForecastController.java
@@ -3,11 +3,20 @@ package ru.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 @Controller
 public class WeatherForecastController {
 
     @GetMapping("forecast")
-    String forecast(){
+    String forecast() throws IOException, InterruptedException {
+
+        //System.out.println(stringHttpResponse.body());
+
         return "forecast";
     }
 }
