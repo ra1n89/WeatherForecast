03e6448b80ec0ea0af049af7095558b9a80156b5
ra1n89nout <prorain88@gmail.com>
2025-01-06 15:59:11 +0500

    feat: add utility class to convert from JSON to Object

---
+++ src/main/java/ru/util/JsonUtil.java
@@ -0,0 +1,27 @@
+package ru.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.experimental.UtilityClass;
+import org.springframework.stereotype.Component;
+import ru.model.WeatherFoundByCity;
+
+@UtilityClass
+public class JsonUtil {
+
+    static public WeatherFoundByCity stringToJson(String jsonString) throws JsonProcessingException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(jsonString);
+
+        String name = jsonNode.get("name").asText();
+        String shortCountry = jsonNode.get("sys").get("country").asText();;
+        int degrees = jsonNode.get("main").get("temp").asInt();;
+        int humidity = jsonNode.get("main").get("humidity").asInt();;
+        String skyDefenition = jsonNode.get("weather").asText("main");;
+        String feelsLike = jsonNode.get("main").get("feels_like").asText();
+
+        return new WeatherFoundByCity(name, shortCountry, degrees, humidity, skyDefenition, feelsLike);
+    }
+}
