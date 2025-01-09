4ce95f7bfe6aca38a292965bb5f9f82f748d7810
Ahmetzyanov <prorain88@gmail.com>
2025-01-09 21:48:40 +0500

    refactor: simplify code

--- src/main/java/ru/interceptor/LoggingInterceptor.java
+++ src/main/java/ru/interceptor/LoggingInterceptor.java
@@ -6,34 +6,34 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 import ru.repository.SessionRepository;
 import ru.repository.entity.UserSession;
+import ru.service.UserValidationService;
+
 import java.sql.Timestamp;
 
 public class LoggingInterceptor implements HandlerInterceptor {
 
     private final SessionRepository sessionRepository;
 
-    public LoggingInterceptor(SessionRepository sessionRepository) {
+    UserValidationService userValidationService;
+
+    public LoggingInterceptor(SessionRepository sessionRepository, UserValidationService userValidationService) {
         this.sessionRepository = sessionRepository;
+        this.userValidationService = userValidationService;
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Cookie[] cookies = request.getCookies();
-        if (cookies == null) {
-            System.out.println("No cookies found.");
-        } else {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("GUID")) {
-                    UserSession userSession = sessionRepository.getById(cookie.getValue());
-                    Timestamp currentTime = new Timestamp(System.currentTimeMillis());
 
-                    if (userSession == null || userSession.getExpiresAt().before(currentTime)) {
-                        //TODO: проверку времени куки вынести в отдельный БД, и там еще удалять эту сессию из БД
-                        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "You are not authorized");
-                        return false;
-                    }
-                    return true;
-                }
+        if (cookies == null) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "You are not authorized");
+            return false;
+        }
+
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("GUID") && userValidationService.isUserAuthorized(cookie.getValue())) {
+                return true;
+
             }
         }
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "You are not authorized");
